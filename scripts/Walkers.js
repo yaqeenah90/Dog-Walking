f2872df632157414ab0c4d3495ae457455256b4e
yaqeenah90 <yaqeenah@icloud.com>
2021-12-02 08:40:53 -0800

    dog walking updates

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,5 +1,21 @@
-//added an s to getWalkerso it could recognize imported file.//
+
 import { getWalkers } from "./database.js"
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("walker")) {
+            const [,walkerId] = itemClicked.id.split("--")
+
+            for (const walker of walkers) {
+                if (walker.id === parseInt(walkerId)) {
+                    window.alert(`${walker.name} services ${walker.city}`)
+                }
+            }
+        }
+    }
+)
+
 
 const walkers = getWalkers()
 
@@ -8,7 +24,8 @@ export const Walkers = () => {
     let walkerHTML = "<ul>"
 
     for (const walker of walkers) {
-        walkerHTML += `<li>${walker.name}</li>`
+        walkerHTML += `<li id="walker--${walker.id}">${walker.name}</li>`
+
     }
 
     walkerHTML += "</ul>"
